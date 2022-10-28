2414b65bab0cfa770c9fed3c61a5ba1cc1981ef9
Andre Felipe <afelipes@gmail.com>
2022-10-28 16:43:07 -0300

    feat: :rocket: O retorno padrão de um objeto Product deve ser um relatório sobre ele

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        7,
+        "Spironolactone",
+        "REMEDYREPACK",
+        "2021-07-17",
+        "2023-11-18",
+        "SM28",
+        "Manter a temperatura ambiente",
+    )
+
+    object_product = new_product.__repr__()
+
+    object_match = """O produto Spironolactone fabricado em 2021-07-17
+ por REMEDYREPACK com validade até 2023-11-18 precisa ser armazenado
+ Manter a temperatura ambiente."""
+
+    assert object_product == object_match.replace("\n", "")
